223286a409f0eff64145ccf0bd9c2a63d6334b10
Aditya Pattnaik <pattnaikaditya1@gmail.com>
2023-10-17 10:45:54 +0530

    usage_of_jsx

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 import react from "react";
 import reactDom from "react-dom/client";
 
-const heading = react.createElement("h1", { id: "heading" }, "Hello World from react");
+const heading = react.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from react"
+);
 const root = reactDom.createRoot(document.getElementById("root"));
-root.render(heading);
-const parent = react.createElement("div", { id: "parent" },
-    [react.createElement("div", { id: "child" },
-        [react.createElement("h1", {}, "I am a H1 tag"), react.createElement("h2", {}, "I am a H2 tag")]
-    ), react.createElement("div", { id: "child" },
-        [react.createElement("h1", {}, "I am a H1 tag"), react.createElement("h2", {}, "I am a H2 tag")]
-    )]);
-root.render(parent);
+//root.render(heading);
+
+const value = 1000;
+//JSX Element
+const headingElement = <h1>Hello World From JSX</h1>;
+
+const Headings = () => <h1>Hello World From JSX</h1>;
+
+
+//JSX Component
+const HeadingComponent = () => (
+  <div id="somethine">
+    <h1 id="head">Hello World From JSX Component</h1>
+    <h2>{value}</h2>
+    {heading}
+    {headingElement}
+    <Headings/>
+  </div>
+);
+root.render(<HeadingComponent />);
